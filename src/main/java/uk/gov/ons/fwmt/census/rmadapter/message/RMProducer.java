08dc235417b9005d5df6ccb54a716203d3697ec4
garethTurner <gareth.turner@ons.gov.uk>
2018-12-13 14:44:46 +0000

    use census response message

--- src/main/java/uk/gov/ons/fwmt/census/rmadapter/message/RMProducer.java
+++ src/main/java/uk/gov/ons/fwmt/census/rmadapter/message/RMProducer.java
@@ -1,9 +1,10 @@
 package uk.gov.ons.fwmt.census.rmadapter.message;
 
+import uk.gov.ons.fwmt.census.rmadapter.data.CensusCaseOutcomeDTO;
 import uk.gov.ons.fwmt.fwmtgatewaycommon.error.CTPException;
 import uk.gov.ons.fwmt.fwmtohsjobstatusnotification.FwmtOHSJobStatusNotification;
 
 public interface RMProducer {
 
-  void sendJobRequestResponse(FwmtOHSJobStatusNotification createJobRequest) throws CTPException;
+  void sendJobRequestResponse(CensusCaseOutcomeDTO createJobRequest) throws CTPException;
 }

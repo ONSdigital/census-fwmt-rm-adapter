b44b640d0216ea1b0f81ec3ab8d06d049651a1c4
Simon <sdiaz1000@gmail.com>
2019-01-14 15:37:58 +0000

    fwmt-665 Added datamap changes and implementation.

--- src/test/java/uk/gov/ons/fwmt/census/rmadapter/helper/ActionInstructionBuilder.java
+++ src/test/java/uk/gov/ons/fwmt/census/rmadapter/helper/ActionInstructionBuilder.java
@@ -2,6 +2,7 @@ package uk.gov.ons.fwmt.census.rmadapter.helper;
 
 import uk.gov.ons.ctp.response.action.message.instruction.ActionAddress;
 import uk.gov.ons.ctp.response.action.message.instruction.ActionCancel;
+import uk.gov.ons.ctp.response.action.message.instruction.ActionContact;
 import uk.gov.ons.ctp.response.action.message.instruction.ActionInstruction;
 import uk.gov.ons.ctp.response.action.message.instruction.ActionRequest;
 import uk.gov.ons.ctp.response.action.message.instruction.ActionUpdate;
@@ -19,6 +20,9 @@ public class ActionInstructionBuilder {
     actionRequest.setSurveyRef("testSurveyRef");
     actionRequest.setReturnByDate("11/11/2000");
 
+    ActionContact contact = new ActionContact();
+    
+    
     actionAddress.setLatitude(BigDecimal.valueOf(1000.00));
     actionAddress.setLongitude(BigDecimal.valueOf(1000.00));
     actionAddress.setLine1("addressLine1");
@@ -28,6 +32,7 @@ public class ActionInstructionBuilder {
 
     actionRequest.setAddress(actionAddress);
     actionInstruction.setActionRequest(actionRequest);
+    actionRequest.setContact(contact);
 
     return actionInstruction;
   }

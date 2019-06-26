82a328ca486d34849aac25ea06a0c9ef96ac0c08
stevescorfield <steve.scorfield@ons.gov.uk>
2019-06-26 15:25:06 +0100

    Added a separate tyr/catch statement for preventing null point exception with coordinator ID

--- src/main/java/uk/gov/ons/census/fwmt/rmadapter/canonical/CanonicalJobHelper.java
+++ src/main/java/uk/gov/ons/census/fwmt/rmadapter/canonical/CanonicalJobHelper.java
@@ -43,17 +43,25 @@ public final class CanonicalJobHelper {
     createJobRequest.setSurveyType(actionRequest.getTreatmentId());
     createJobRequest.setEstablishmentType(actionAddress.getEstabType());
 
-    if (actionAddress.getCountry() == "N") {
-      if (!StringUtils.isEmpty(actionRequest.getFieldOfficerId()) &&
-          !StringUtils.isEmpty(actionRequest.getCoordinatorId())) {
+    if (actionAddress.getCountry().equals("N")) {
+      if (!StringUtils.isEmpty(actionRequest.getFieldOfficerId())) {
       createJobRequest.setMandatoryResource(processMandatoryResource(actionRequest));
       } else {
         throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
-            "A NISRA request was sent but did not include a field officer ID and/or coordinator ID for case {}"
-            , actionRequest.getCaseId());
+            "A NISRA request was sent but did not include a field officer ID and/or coordinator ID for case {}",
+            actionRequest.getCaseId());
       }
     }
-    createJobRequest.setCoordinatorId(actionRequest.getCoordinatorId());
+
+    // Coordinator ID should always be present but if it's not then a null pointer exception would be thrown. Added a
+    // Try/Catch to allow user to know what went wrong
+    try {
+      createJobRequest.setCoordinatorId(actionRequest.getCoordinatorId());
+    } catch (Exception e) {
+      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
+          "A case request was sent that did not include a coordinator ID for case {}",
+          actionRequest.getCaseId());
+    }
     createJobRequest.setActionType(actionRequest.getActionType());
 
     Contact contact = getContact(actionContact, actionAddress);

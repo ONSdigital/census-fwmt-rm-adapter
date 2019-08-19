291dbd3084439798c7e621e4aaa4e53abd0d1d58
SteveScorfield <steve.scorfield@ons.gov.uk>
2019-08-19 13:45:01 +0100

    Added REDIS logging checks for household

--- src/main/java/uk/gov/ons/census/fwmt/rmadapter/service/impl/JobCacheManagerImpl.java
+++ src/main/java/uk/gov/ons/census/fwmt/rmadapter/service/impl/JobCacheManagerImpl.java
@@ -3,6 +3,7 @@ package uk.gov.ons.census.fwmt.rmadapter.service.impl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import uk.gov.ons.census.fwmt.rmadapter.config.RedisUtil;
 import uk.gov.ons.census.fwmt.rmadapter.redis.HouseholdRequestEntity;
 import uk.gov.ons.census.fwmt.rmadapter.service.JobCacheManager;
@@ -17,16 +18,18 @@ public class JobCacheManagerImpl implements JobCacheManager {
   @Override
   public HouseholdRequestEntity cacheCreateHouseholdRequest(HouseholdRequestEntity householdRequestEntity) {
     redisUtil.putValue(householdRequestEntity.getCaseId(), householdRequestEntity);
+    String check = redisUtil.getValue(householdRequestEntity.getCaseId()).toString();
     log.info("Placed the following in cache: " +  householdRequestEntity.toString());
     return householdRequestEntity;
   }
 
   @Override
   public HouseholdRequestEntity getCachedHouseholdCaseId(String caseId) {
-    HouseholdRequestEntity householdRequestEntity = redisUtil.getValue(caseId);
-    log.info("Received object from cache: " + householdRequestEntity.toString());
-
-    return householdRequestEntity;
+    if (!StringUtils.isEmpty(redisUtil.getValue(caseId))) {
+      HouseholdRequestEntity householdRequestEntity = redisUtil.getValue(caseId);
+      log.info("Received object from cache: " + householdRequestEntity.toString());
+    }
+    return null;
   }
 }
 

875eae45e98ced3e525276b245a2124d4dcf8398
SteveScorfield <steve.scorfield@ons.gov.uk>
2019-10-23 13:26:36 +0100

    Added logging for RabbitMQ and Redis health checks

--- src/main/java/uk/gov/ons/census/fwmt/rmadapter/config/GatewayEventsConfig.java
+++ src/main/java/uk/gov/ons/census/fwmt/rmadapter/config/GatewayEventsConfig.java
@@ -14,10 +14,14 @@ public class GatewayEventsConfig {
   public static final String RM_CREATE_REQUEST_RECEIVED = "RM_CREATE_REQUEST_RECEIVED";
   public static final String RM_UPDATE_REQUEST_RECEIVED = "RM_UPDATE_REQUEST_RECEIVED";
   public static final String RM_CANCEL_REQUEST_RECEIVED = "RM_CANCEL_REQUEST_RECEIVED";
+  public static final String RABBIT_QUEUE_UP = "RABBIT_QUEUE_UP";
+  public static final String REDIS_SERVICE_UP = "REDIS_SERVICE_UP";
 
   public static final String INVALID_ACTION_INSTRUCTION = "INVALID_ACTION_INSTRUCTION";
   public static final String FAILED_TO_UNMARSHALL_ACTION_INSTRUCTION = "FAILED_TO_UNMARSHALL_ACTION_INSTRUCTION";
   public static final String FAILED_TO_MARSHALL_CANONICAL = "FAILED_TO_MARSHALL_CANONICAL";
+  public static final String RABBIT_QUEUE_DOWN = "RABBIT_QUEUE_DOWN";
+  public static final String REDIS_SERVICE_DOWN = "REDIS_SERVICE_DOWN";
 
   @Bean
   public GatewayEventManager gatewayEventManager() {
@@ -25,10 +29,10 @@ public class GatewayEventsConfig {
     gatewayEventManager.setSource(Application.APPLICATION_NAME);
     gatewayEventManager.addEventTypes(
         new String[] {CANONICAL_CREATE_SENT, CANONICAL_UPDATE_SENT, CANONICAL_CANCEL_SENT, RM_CREATE_REQUEST_RECEIVED,
-            RM_UPDATE_REQUEST_RECEIVED, RM_CANCEL_REQUEST_RECEIVED});
+            RM_UPDATE_REQUEST_RECEIVED, RM_CANCEL_REQUEST_RECEIVED, RABBIT_QUEUE_UP, REDIS_SERVICE_UP});
     gatewayEventManager.addErrorEventTypes(
         new String[] {INVALID_ACTION_INSTRUCTION, FAILED_TO_UNMARSHALL_ACTION_INSTRUCTION,
-            FAILED_TO_MARSHALL_CANONICAL});
+            FAILED_TO_MARSHALL_CANONICAL, RABBIT_QUEUE_DOWN, REDIS_SERVICE_DOWN});
     return gatewayEventManager;
   }
 }

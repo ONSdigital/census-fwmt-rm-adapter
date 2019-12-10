0cc9a89cd1de8f5c2abf70d25665f9d6565762ab
SteveScorfield <steve.scorfield@ons.gov.uk>
2019-12-10 09:41:29 +0000

    Removed additional caching call and added error event when case ID is found in Redis

--- src/main/java/uk/gov/ons/census/fwmt/rmadapter/service/impl/RMAdapterServiceImpl.java
+++ src/main/java/uk/gov/ons/census/fwmt/rmadapter/service/impl/RMAdapterServiceImpl.java
@@ -6,79 +6,83 @@ import org.springframework.stereotype.Component;
 import uk.gov.ons.census.fwmt.common.error.GatewayException;
 import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
 import uk.gov.ons.census.fwmt.rmadapter.canonical.CanonicalJobHelper;
+import uk.gov.ons.census.fwmt.rmadapter.config.GatewayEventsConfig;
 import uk.gov.ons.census.fwmt.rmadapter.message.GatewayActionProducer;
 import uk.gov.ons.census.fwmt.rmadapter.redis.HouseholdStore;
 import uk.gov.ons.census.fwmt.rmadapter.service.RMAdapterService;
 import uk.gov.ons.ctp.response.action.message.instruction.ActionInstruction;
 
-import static uk.gov.ons.census.fwmt.rmadapter.config.GatewayEventsConfig.CANONICAL_CANCEL_SENT;
-import static uk.gov.ons.census.fwmt.rmadapter.config.GatewayEventsConfig.CANONICAL_CREATE_SENT;
-import static uk.gov.ons.census.fwmt.rmadapter.config.GatewayEventsConfig.CANONICAL_UPDATE_SENT;
-import static uk.gov.ons.census.fwmt.rmadapter.config.GatewayEventsConfig.INVALID_ACTION_INSTRUCTION;
+import static uk.gov.ons.census.fwmt.rmadapter.config.GatewayEventsConfig.*;
 
 @Slf4j
 @Component
 public class RMAdapterServiceImpl implements RMAdapterService {
 
-  @Autowired
-  private GatewayEventManager gatewayEventManager;
+    @Autowired
+    private GatewayEventManager gatewayEventManager;
 
-  @Autowired
-  private GatewayActionProducer jobServiceProducer;
+    @Autowired
+    private GatewayActionProducer jobServiceProducer;
 
-  @Autowired
-  private HouseholdStore householdStore;
+    @Autowired
+    private HouseholdStore householdStore;
 
-  @Autowired
-  private CanonicalJobHelper canonicalJobHelper = new CanonicalJobHelper();
+    @Autowired
+    private CanonicalJobHelper canonicalJobHelper = new CanonicalJobHelper();
 
-  public void sendJobRequest(ActionInstruction actionInstruction) throws GatewayException {
-    if (actionInstruction.getActionRequest() != null) {
-      sendCreateMessage(actionInstruction);
-    } else if (actionInstruction.getActionCancel() != null) {
-      createCancelMessage(actionInstruction);
-    } else if (actionInstruction.getActionUpdate() != null) {
-      createUpdateMessage(actionInstruction);
-    } else {
-      String msg = "No matching request was found";
-      String unknown = "Unknown caseId";
-      gatewayEventManager.triggerErrorEvent(this.getClass(), msg, unknown, INVALID_ACTION_INSTRUCTION);
-      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, msg);
+    public void sendJobRequest(ActionInstruction actionInstruction) throws GatewayException {
+        if (actionInstruction.getActionRequest() != null) {
+            sendCreateMessage(actionInstruction);
+        } else if (actionInstruction.getActionCancel() != null) {
+            createCancelMessage(actionInstruction);
+        } else if (actionInstruction.getActionUpdate() != null) {
+            createUpdateMessage(actionInstruction);
+        } else {
+            String msg = "No matching request was found";
+            String unknown = "Unknown caseId";
+            gatewayEventManager.triggerErrorEvent(this.getClass(), msg, unknown, INVALID_ACTION_INSTRUCTION);
+            throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, msg);
+        }
     }
-  }
 
-  private void createUpdateMessage(ActionInstruction actionInstruction) throws GatewayException {
-    if (householdStore.retrieveCache(actionInstruction.getActionUpdate().getCaseId()) == null)
-      return;
-
-    if (actionInstruction.getActionUpdate().getAddressType().equals("HH")) {
-      jobServiceProducer.sendMessage(canonicalJobHelper.newUpdateJob(actionInstruction));
-      gatewayEventManager.triggerEvent(actionInstruction.getActionUpdate().getCaseId(), CANONICAL_UPDATE_SENT);
-    } else {
-      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, "Valid address type not found");
+    private void createUpdateMessage(ActionInstruction actionInstruction) throws GatewayException {
+        if (householdStore.retrieveCache(actionInstruction.getActionUpdate().getCaseId()) == null) {
+            String msg = "Unable to find cached data for update case";
+            String caseId = actionInstruction.getActionUpdate().getCaseId();
+            gatewayEventManager.triggerErrorEvent(this.getClass(), msg, caseId,
+                    GatewayEventsConfig.REDIS_NO_RECORD_FOUND);
+        } else {
+            if (actionInstruction.getActionUpdate().getAddressType().equals("HH")) {
+                jobServiceProducer.sendMessage(canonicalJobHelper.newUpdateJob(actionInstruction));
+                gatewayEventManager.triggerEvent(actionInstruction.getActionUpdate().getCaseId(), CANONICAL_UPDATE_SENT);
+            } else {
+                throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, "Valid address type not found");
+            }
+        }
     }
-  }
 
-  private void createCancelMessage(ActionInstruction actionInstruction) throws GatewayException {
-    if (householdStore.retrieveCache(actionInstruction.getActionCancel().getCaseId()) == null)
-      return;
+    private void createCancelMessage(ActionInstruction actionInstruction) throws GatewayException {
+        if (householdStore.retrieveCache(actionInstruction.getActionCancel().getCaseId()) == null) {
+            String msg = "Unable to find cached data for cancel case";
+            String caseId = actionInstruction.getActionCancel().getCaseId();
+            gatewayEventManager.triggerErrorEvent(this.getClass(), msg, caseId,
+                    GatewayEventsConfig.REDIS_NO_RECORD_FOUND);
+        } else {
+            if (actionInstruction.getActionCancel().getAddressType().equals("HH")) {
 
-    if (householdStore.retrieveCache(actionInstruction.getActionCancel().getCaseId()) != null) {
-      if (actionInstruction.getActionCancel().getAddressType().equals("HH")) {
-
-        jobServiceProducer.sendMessage(canonicalJobHelper.newCancelJob(actionInstruction));
-        gatewayEventManager
-            .triggerEvent(actionInstruction.getActionCancel().getCaseId(), CANONICAL_CANCEL_SENT, "Case Ref",
-                actionInstruction.getActionCancel().getCaseRef());
-      } else {
-        throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, "Valid address type not found");
-      }
+                jobServiceProducer.sendMessage(canonicalJobHelper.newCancelJob(actionInstruction));
+                gatewayEventManager
+                        .triggerEvent(actionInstruction.getActionCancel().getCaseId(), CANONICAL_CANCEL_SENT, "Case Ref",
+                                actionInstruction.getActionCancel().getCaseRef());
+            } else {
+                throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR, "Valid address type not found");
+            }
+        }
     }
-  }
 
-  private void sendCreateMessage(ActionInstruction actionInstruction) throws GatewayException {
-    householdStore.cacheJob(actionInstruction.getActionRequest().getCaseId());
-    jobServiceProducer.sendMessage(canonicalJobHelper.newCreateJob(actionInstruction));
-    gatewayEventManager.triggerEvent(actionInstruction.getActionRequest().getCaseId(), CANONICAL_CREATE_SENT);
-  }
+    private void sendCreateMessage(ActionInstruction actionInstruction) throws GatewayException {
+        householdStore.cacheJob(actionInstruction.getActionRequest().getCaseId());
+        jobServiceProducer.sendMessage(canonicalJobHelper.newCreateJob(actionInstruction));
+        gatewayEventManager.triggerEvent(actionInstruction.getActionRequest().getCaseId(), CANONICAL_CREATE_SENT);
+    }
 }
